ac61ea40f34811b50000c8497acca53117293120
Bruce-shy <zhouzihan341308@163.com>
2020-04-19 20:28:45 +0800

    支付宝口碑界面

---
+++ h5/public_praise/index.js
@@ -0,0 +1,3 @@
+var menu1 = document.getElementsByClassName('menu1');
+var menu_inner1 = document.getElementsByClassName('menu-inner1');
+var menu_inner2 = document.getElementsByClassName('menu-inner2');
---
+++ h5/public_praise/index.ts
@@ -0,0 +1,2 @@
+const menu_inner1 = document.getElementsByClassName('menu-inner1');
+const menu_inner2 = document.getElementsByClassName('menu-inner2');
