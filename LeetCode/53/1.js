d7db12e0307f640d3a6dd0b19b03b14ac04815cc
Bruce-shy <zhouzihan341308@163.com>
2020-05-04 22:08:31 +0800

    力扣第53题

---
+++ LeetCode/53/1.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let length = nums.length;
+    let val = 0;
+console.log(nums);
+};
+maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
