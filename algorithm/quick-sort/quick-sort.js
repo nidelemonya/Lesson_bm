02fb6a86b22941d0350200c2bafcdaee9c33b062
Bruce-shy <zhouzihan341308@163.com>
2020-05-27 22:13:28 +0800

    分而治之 快速排序 partition 小算法

---
+++ algorithm/quick-sort/quick-sort.js
@@ -0,0 +1,52 @@
+// 双指针法 升序
+function partition(nums, left, right) {
+    // 如果左指针大于右指针 返回
+    if (left >= right) return;
+    let i = left;
+    let j = right;
+    // 基准值选择数组的第一个元素
+    let provit = nums[left];
+    // 如果是最左边为基准的话，那就得右指针先开始找
+    // 如果是最右边为基准的话，那就得左指针先开始找
+    while (left < right) {
+        // 右指针找到比基准值小的数 换到前面去
+        // 左指针找到比基准值大的数 换到后面去
+        // left 和 right 指针互相换 
+        // right 指针 替换 left 位置
+        // left 替换 right 位置
+
+        // 1. 右边扫描, 比基准值小的数, 如果这个数比基准值大, 一直往前走
+        while (left < right && nums[right] >= provit) {
+            right--;
+        }
+        nums[left] = nums[right];
+        // 2. 左边扫描, 比基准值大的数, 如果这个数比基准值小, 一直往后走
+        while (left < right && nums[left] <= provit) {
+            left++;
+        }
+        nums[right] = nums[left];
+    }
+    nums[left] = provit;
+    // 取到基准值位置 区分左右区间
+    // return left;
+    partition(nums, i, left - 1);
+    partition(nums, left + 1, j);
+}
+
+const arr = [8, 9, 7, -1, 5, 48, 68, -3, -2];
+// [8, 9, 7, -1, 5]
+// let provit = 8
+// left: 0 right: 4[5, 9, 7, 5]
+// [5, 9, 7, -1, 9]
+// [5, -1, 7, -1, 9]
+// [5, -1, 7, 8, 9]
+
+// 大的分区 伪代码
+const quickSort = function (arr) {
+    partition(arr, 0, arr.length - 1);
+}
+quickSort(arr);
+console.log(arr);
+
+// partition(nums, 0, (nums.length - 1)>>1)
+// partition((nums.length - 1)>>1, 0, nums.length - 1)
---
+++ algorithm/quick-sort/ryf.js
@@ -0,0 +1,18 @@
+const qs = (arr) => {
+    if (arr.length <= 1) return arr;
+    let left = [];
+    let right = [];
+    let provit = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < provit) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+    return [...qs(left), provit, ...qs(right)];
+  }
+  const arr = [0, 1, -1, 3, 99, -4, 6];
+  // 没有原地交换
+  // 但是比较好理解
+  console.log(qs(arr));
