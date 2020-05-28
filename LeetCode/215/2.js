a91d7b7800f892a0ad9e8ac171649b4f002bf5c4
Bruce-shy <zhouzihan341308@163.com>
2020-05-28 22:12:44 +0800

    快速排序 数组第K大元素

---
+++ LeetCode/215/2.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+    let i = 0,
+        j = nums.length - 1;
+    // 基准 -> index
+    let index = partition(nums, i, j);
+    // 判断基准值 是不是 第k大的位置
+    // [8, 9, 7, -1, 5] k = 2 
+    //partition: [9, 8, 7, -1, 5]
+    // 0 1 2 3
+    while ((index+1) !== k) {
+        if (index < k) {
+            i = index + 1;
+        } else {
+            j = index - 1;
+        }
+        index = partition(nums, i, j);
+    }
+    return nums[index];
+};
+// 降序
+function partition(nums, left, right) {
+    // 如果左指针大于右指针 返回
+    let i = left;
+    let j = right;
+    // 基准值选择数组的第一个元素
+    let provit = nums[left];
+    // 如果是最左边为基准的话，那就得右指针先开始找
+    // 如果是最右边为基准的话，那就得左指针先开始找
+    while (left < right) {
+        // 右指针找到比基准值大的数 换到前面去
+        // 左指针找到比基准值小的数 换到后面去
+        // left 和 right 指针互相换 
+        // right 指针 替换 left 位置
+        // left 替换 right 位置
+
+        // 1. 右边扫描, 比基准值大的数, 如果这个数比基准值小, 一直往前走
+        while (left < right && nums[right] <= provit) {
+            right--;
+        }
+        nums[left] = nums[right];
+        // 2. 左边扫描, 比基准值小的数, 如果这个数比基准值大, 一直往后走
+        while (left < right && nums[left] >= provit) {
+            left++;
+        }
+        nums[right] = nums[left];
+    }
+    nums[left] = provit;
+    // 取到基准值位置 区分左右区间
+    return left;
+}
