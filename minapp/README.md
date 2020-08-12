029775e6e5869e09a7529c9fc2a80fe17e41e990
Bruce-shy <zhouzihan341308@163.com>
2020-08-12 14:37:21 +0800

    小程序

---
+++ minapp/README.md
@@ -0,0 +1,22 @@
+# 云开发 quickstart
+
+这是云开发的快速启动指引，其中演示了如何上手使用云开发的三大基础能力：
+
+- 数据库：一个既可在小程序前端操作，也能在云函数中读写的 JSON 文档型数据库
+- 文件存储：在小程序前端直接上传/下载云端文件，在云开发控制台可视化管理
+- 云函数：在云端运行的代码，微信私有协议天然鉴权，开发者只需编写业务逻辑代码
+
+## 参考文档
+
+- [云开发文档](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/basis/getting-started.html)
+
+1. MVVM React JSX WXML
+2. setData setState onLoad
+3. taro
+4. 前端框架 antd => vant 有赞
+5. 全栈 云数据库 云文件处理
+
+在小程序中 div 就是 view
+{{name}} 表示数据的引用
+
+  "style": "v2" 去掉 才可以自定义样式
---
+++ minapp/miniprogram/pages/index/index.js
@@ -0,0 +1,158 @@
+//index.js
+const app = getApp()
+const db = wx.cloud.database()
+const photos = db.collection('photos') // 拿出数据表
+Page({
+  data: {
+    name: '你的lemon呀',
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  upload() {
+    console.log("上传图片")
+    wx.chooseImage({
+      count:9,
+      sizeType:['original', 'compressed'],
+      sourceType:['album', 'camera'],
+      success:res => {
+        // console.log(res.tempFilePaths);
+        for (let filePath of res.tempFilePaths) {
+          wx.cloud.uploadFile({
+            cloudPath:Math.floor(Math.random()*100000).toString() + '.png',
+            filePath: filePath,
+            success: res => {
+              console.log(res);
+              photos.add({
+                data: {
+                  image: res.fileID
+                }
+              })
+              .then(res => {
+                wx.showToast({
+                  title: '上传成功',
+                  icon: 'success'
+                })
+              })
+            }
+          })
+        }
+      },
+    })
+  },
+  onLoad: function() {
+    setTimeout(() => {
+      this.setData({
+        name: '是你的lemon呀'
+      },2000)
+    })
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+  // 上传图片
+  doUpload: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+
+        wx.showLoading({
+          title: '上传中',
+        })
+
+        const filePath = res.tempFilePaths[0]
+        
+        // 上传图片
+        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        wx.cloud.uploadFile({
+          cloudPath,
+          filePath,
+          success: res => {
+            console.log('[上传文件] 成功：', res)
+
+            app.globalData.fileID = res.fileID
+            app.globalData.cloudPath = cloudPath
+            app.globalData.imagePath = filePath
+            
+            wx.navigateTo({
+              url: '../storageConsole/storageConsole'
+            })
+          },
+          fail: e => {
+            console.error('[上传文件] 失败：', e)
+            wx.showToast({
+              icon: 'none',
+              title: '上传失败',
+            })
+          },
+          complete: () => {
+            wx.hideLoading()
+          }
+        })
+
+      },
+      fail: e => {
+        console.error(e)
+      }
+    })
+  },
+
+})
