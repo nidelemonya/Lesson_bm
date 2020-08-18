6bc396344a4ac545a0d71e6e8868c865aaea4dbc
“Bruce-shy <zhouzihan341308@163.com>
2020-08-18 23:40:42 +0800

    加1

---
+++ LeetCode/43/1.js
@@ -0,0 +1,22 @@
+const multiply = (num1, num2) => {
+    const len1 = num1.length;
+    const len2 = num2.length;
+    const pos = new Array(len1 + len2).fill(0); // fill() 方法用于将一个固定值替换数组的元素
+  
+    for (let i = len1 - 1; i >= 0; i--) {
+      const n1 = +num1[i];
+      for (let j = len2 - 1; j >= 0; j--) {
+        const n2 = +num2[j];
+        const multi = n1 * n2;
+        const sum = pos[i + j + 1] + multi;
+  
+        pos[i + j + 1] = sum % 10;
+        pos[i + j] += sum / 10 | 0;
+      }
+    }
+    let resStr = pos.join('');
+    while (resStr[0] == '0') {
+      resStr = resStr.substring(1);
+    }
+    return resStr.length ? resStr : '0';
+  }
--- LeetCode/66/1.js
+++ LeetCode/66/1.js
@@ -3,9 +3,20 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
+    const len = digits.length;
+    for(let i = len - 1; i >= 0; i--) {
+        digits[i]++;
+        digits[i] %= 10;
+        if(digits[i]!=0)
+            return digits;
+    }
+    digits.unshift(1);
+    return digits;
+}
 
-};
+console.log(plusOne([9]))
+// console.log(plusOne([9]))
 // console.log(plusOne([1,2,3])) // [1,2,4]
 // console.log(plusOne([4,3,2,1])) // [4,3,2,2]
-console.log(plusOne([4,3,2,9,9])) // [4,3,3,0]
+console.log(plusOne([4,3,2,9,9])) // [4,3,3,0,0]
 // console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
