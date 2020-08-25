d564785fdd284afc9a18d7c77a4560ee190bf5a7
“Bruce-shy <zhouzihan341308@163.com>
2020-08-25 15:27:09 +0800

    vue element 源码分析

---
+++ element-code/README.md
@@ -0,0 +1,28 @@
+- vue-cli 4. vue3.0 变化太大 拥抱了react
+- vue create project-name
+    vue 2.0 vue init webpack project-name 传统 现在流行
+    vue 3.0 vue create project-name
+    - 原生支持 ts
+    - PWA 渐进式网页应用
+    - Router Vuex 全家桶
+    - CSS Pre-processors stylus vw autoprefixer
+        编译 sass/stylus 插件系统 autoprefixer 前缀
+        postcss-px-to-viewport cssnext
+    - linter 代码风格确保文件 ; " " '' tab
+        eslint 保证大家代码风格一致的
+        不用的变量不要定义， 要删除干净
+        对齐代码
+        "" ''
+        js 的结尾不加;
+    - unit Testing E2E 测试
+
+
+
+
+Manually select features
+Choose Vue version, Babel, Router, Vuex, CSS Pre-processors
+2.x
+history mode for router? Yes 
+Pick a CSS pre-processor Stylus
+Where do you prefer placing config for Babel, ESLint, etc.? In package.json
+Save this as a preset for future projects? No
---
+++ element-code/src/components/ElButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- slot 插槽 -->
+  <button
+    @click="handleClick"
+    class="el-button"
+    :class="[
+        type?'el-button--'+type:'',
+        {
+            'is-round': round,
+            'is-circle': circle
+        }
+    ]"
+  >
+    <!-- {{type}} - {{tit}} -->
+    <!-- 动态类名绑定 -->
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+// props 类型校验 解构
+// vue 怎么做？
+
+export default {
+  data() {
+    return {
+      tit: "hello",
+    };
+  },
+  props: {
+    type: { // 传入的props的名字
+      type: String, // 类型名
+      default: "default",
+    },
+    round: Boolean, // 简略写法
+    // round 传了 就为 true 不传为 false
+    // round : {
+    //     type:Boolean,
+    //     default: false
+    // }
+    circle:Boolean
+  },
+  created() {
+    // 生命周期 挂载阶段
+    console.log(this.type, this.tit, this.round);
+  },
+  methods: {
+      handleClick(event) {
+        this.$emit('click', event)
+      }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+vab
