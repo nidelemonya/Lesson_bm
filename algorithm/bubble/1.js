6c58b44e2f676486a8aaaa7eccfd4e8bb95dc0ea
周子寒 <zhouzihan341308@163.com>
2021-01-25 20:26:32 +0800

    每日学习

--- algorithm/bubble/1.js
+++ algorithm/bubble/1.js
@@ -2,8 +2,8 @@ let arr = [1, 4, 5, 6, 7, 8, 9, 3, 2]
 
 function bubble(array) {
     let n = array.length - 1;
-    for (let i = 1; i < n; i++) {
-        for (let j = 0; j < n - i; j++) {
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j <= n - i; j++) {
             if (array[j + 1] > array[j]) {
                 let t = array[j];
                 array[j] = array[j + 1];
